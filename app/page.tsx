e5d4e71996c886d439c3fc3809cc103c9f36e148
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2026-06-04 13:40:52 +0200

    display collection data

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,39 @@
 import Link from "next/link";
 import { createClient } from "./lib/supabase/server";
+import { Button } from '@base-ui/react/button'
 
 export default async function Home() {
   const supabase = await createClient();
-  const { data: cohorts, error } = await supabase.from("cohorts").select("id, name")
+  const { data: cohorts, error: cohortsError } = await supabase.from("cohorts").select("id, name, slug")
+  const { data: talents, error: talentsError } = await supabase.from("talents").select("id, fullname, slug")
+  const { data: locations, error: locationsError } = await supabase.from("locations").select("id, city, country")
+  const { data: programs, error: programsError } = await supabase.from("programs").select("id, name")
+  const { data: capabilities, error: capabilitiesError } = await supabase.from("capabilities").select("id, name")
 
   return (
     <div>
       <h1 className="text-2xl ml-10 mt-10 mb-5 font-semibold">Home</h1>
-      <div className="ml-10 flex flex-col text-blue-600">
-        <Link href="/talent/thabo-mokoena">Talent Profile</Link>
-        <div className="my-5 flex flex-col">
-          {cohorts?.map(i => <Link href={`/cohort/${i.id}`}>{i.name}</Link>)}
+      <div className="ml-10 flex gap-15">
+        <div>
+          <h2 className="text-xl">Talents</h2>
+          {talents?.map(i => <Link className="text-gray-500 underline block" key={i.id} href={`/talent/${i.slug}`}>{i.fullname}</Link>)}
         </div>
-
+        <div>
+          <h2 className="text-xl">Cohorts</h2>
+          {cohorts?.map(i => <Link className="text-gray-500 underline block" key={i.id} href={`/cohort/${i.slug}`}>{i.name}</Link>)}
+        </div>
+        <div>
+          <h2 className="text-xl">Locations</h2>
+          {locations?.map(i => <Link className="text-gray-500 block" key={i.id} href="#">{i.city}</Link>)}
+        </div>
+        <div>
+          <h2 className="text-xl">Programs</h2>
+          {programs?.map(i => <Link className="text-gray-500 block" key={i.id} href="#">{i.name}</Link>)}
+        </div>
+        <div>
+          <h2 className="text-xl">Capabilities</h2>
+          {capabilities?.map(i => <Link className="text-gray-500 block" key={i.id} href="#">{i.name}</Link>)}
+        </div>
       </div>
     </div>
   );
--- app/talent/[slug]/page.tsx
+++ app/talent/[slug]/page.tsx
@@ -1,12 +1,15 @@
-import Link from "next/link";
+import { createClient } from "@/app/lib/supabase/server";
 
-export default async function Page() {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params
+    const supabase = await createClient();
+    const { data: talent, error: talentError } = await supabase.from("talents").select("id, fullname, bio").eq("slug", slug).single()
     return (
         <div>
-            <h1 className="text-2xl ml-10 mt-10 mb-5 font-semibold">Talent Profile</h1>
-            <div className="ml-10 flex flex-col text-blue-600">
-                <Link href="/">Home</Link>
-                <Link href="/cohort/franjipani-2026">Cohort Selection</Link>
+            <h1 className="text-2xl ml-10 mt-10 mb-5 font-semibold">Talent</h1>
+            <div className="ml-10 flex flex-col">
+                <h2>{talent?.fullname}</h2>
+                <p>{talent?.bio}</p>
             </div>
         </div>
     )
