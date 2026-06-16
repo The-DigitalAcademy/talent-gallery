cb8178d81eb6b7d4a6407276e9fa96066900bdee
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2026-06-16 12:52:48 +0200

    Added CUD functionality for  a Talent's endorsements - created insert & delete forms - created insert & delete server actions

--- app/admin/(dashboard)/collections/talents/[id]/page.tsx
+++ app/admin/(dashboard)/collections/talents/[id]/page.tsx
@@ -5,6 +5,7 @@ import BasicInfoForm from "../forms/basic-info-form";
 import EnrolmentForm from "../forms/enrolment-form";
 import URLsForm from "../forms/urls-form";
 import WorkExperienceForm from "../forms/work-experience-form";
+import EndorsementsForm from "../forms/endorsements-form";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
@@ -19,6 +20,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     const { data: programs, error: programsError } = await supabase.from("programs").select()
     const { data: statuses, error: statusesError } = await supabase.from("talent_statuses").select()
     const { data: workExperiences, error: workExperiencesError } = await supabase.from("work_experiences").select().eq("talent_id", id)
+    const { data: endorsements, error: endorsementsError } = await supabase.from("endorsements").select().eq("talent_id", id)
 
     const enrolmentData = {
         cohorts: cohorts || [],
@@ -53,6 +55,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
                 <EnrolmentForm values={enrolmentValues} data={enrolmentData} />
                 <URLsForm values={urlValues} />
                 <WorkExperienceForm talentId={talent?.id} workExperiences={workExperiences!} />
+                <EndorsementsForm talentId={talent?.id} endorsements={endorsements!} />
             </div >
         </div >
     )
