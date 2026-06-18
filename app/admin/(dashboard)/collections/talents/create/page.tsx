5b43665499114a3af3469e182891c73b3f5a1a41
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2026-06-18 21:03:23 +0200

    add form placeholder components to the create talent page

--- app/admin/(dashboard)/collections/talents/create/page.tsx
+++ app/admin/(dashboard)/collections/talents/create/page.tsx
@@ -3,8 +3,15 @@ import Link from "next/link";
 import BasicInfoForm from "../_forms/basic-info-form";
 import EnrolmentFormPlaceholder from "@/components/admin/enrolment-form-placeholder";
 import ProfileLinksFormPlaceholder from "@/components/admin/profile-links-form-placeholder";
+import ProjectsFormPlaceholder from "@/components/admin/projects-form-placeholder";
+import EndorsementsFormPlaceholder from "@/components/admin/endorsements-form-placeholder";
+import WorkExperienceFormPlaceholder from "@/components/admin/work-experience-form-placeholder";
+import CapabilitiesFormPlaceholder from "@/components/admin/capabilities-form-placeholder";
+import { createClient } from "@/app/lib/supabase/server";
 
 export default async function Page() {
+    const supabase = await createClient();
+    const { data: capabilities, error: capabilitiesError } = await supabase.from("capabilities").select()
     return (
         <div>
             <div className="mb-5">
@@ -17,6 +24,10 @@ export default async function Page() {
                 <BasicInfoForm />
                 <EnrolmentFormPlaceholder />
                 <ProfileLinksFormPlaceholder />
+                <CapabilitiesFormPlaceholder capabilities={capabilities!} />
+                <WorkExperienceFormPlaceholder />
+                <EndorsementsFormPlaceholder />
+                <ProjectsFormPlaceholder />
             </div >
         </div >
     )
---
+++ components/admin/projects-form-placeholder.tsx
@@ -0,0 +1,30 @@
+export default function ProjectsFormPlaceholder() {
+    return (
+        <div>
+            <h2 className="mb-2 font-semibold">Projects</h2>
+            <div className="w-full border border-gray-200 p-6 bg-white rounded-lg">
+                <div className="grid grid-cols-3 gap-7">
+                    <div
+                        className="flex flex-col gap-2 border border-gray-200 p-3 rounded-lg">
+                        <div className="border w-full text-gray-300 rounded-lg h-8 outline-0 border-gray-200 flex justify-between items-center gap-3 leading-none whitespace-nowrap text-sm pl-2 pr-1 font-normal">
+                            Project Name
+                        </div>
+                        <div className="border w-full h-25 text-gray-300 rounded-lg h-8 outline-0 border-gray-200 gap-3 leading-none text-sm p-2 font-normal">
+                            Description
+                        </div>
+                        <button
+                            disabled
+                            type="button"
+                            className="rounded-xl justify-center border border-gray-200 ml-auto text-sm text-gray-300 px-3 h-8 flex gap-1 items-center"
+                        >
+                            Add
+                        </button>
+                    </div>
+                    <div className="flex flex-col gap-3 col-span-2 overflow-y-scroll max-h-55 pr-5">
+                        <div className="w-full text-sm text-gray-300 h-full flex items-center justify-center">No Projects</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
