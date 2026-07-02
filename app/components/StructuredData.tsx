a8586d5d126a746de7a8382f50eb2558999df94a
TommyTeaVee <fihlatv@gmail.com>
2026-07-02 05:40:22 +0200

    fixed linked in for social graph

--- app/components/StructuredData.tsx
+++ app/components/StructuredData.tsx
@@ -5,10 +5,10 @@ export default function StructuredData() {
     "@type": "Organization",
     "name": "Shaper",
     "url": "https://shaper.co.za",
-    "logo": "https://shaper.co.za/logo.png",
+    "logo": "https://shaper.co.za/wp-content/uploads/2025/05/Shaper-Full-Logo-Horizontal-with-new-subtext-scaled.png",
     "description": "Discover and connect with top-tier, broadcast-ready specialized creative professionals and engineering talents.",
     "sameAs": [
-      "https://www.linkedin.com/company/shaper-talent", // Adjust if you have a specific social link
+      "https://www.linkedin.com/company/shaper-talent-southafrica", 
     ]
   };
 
