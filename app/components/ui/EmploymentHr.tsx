38af4e1bf8054c1f5738709fd3dc882b17b981f7
kg-manana <kgomotso@thedigitalacademy.co.za>
2026-07-24 08:35:56 +0200

    position in hr component

--- app/components/ui/EmploymentHr.tsx
+++ app/components/ui/EmploymentHr.tsx
@@ -4,10 +4,11 @@ interface EmploymentStatusProps {
   bgColor: string;
   height: string;
   width?: string;
+  position?: string;
 }
 
-export default function EmploymentHr({ bgColor, height, width = "w-full" }: EmploymentStatusProps) {
+export default function EmploymentHr({ bgColor, height, width = "w-full", position }: EmploymentStatusProps) {
     return(
-        <div className={cn("rounded-[3px]", bgColor, height, width)}/>
+        <div className={cn("rounded-[3px]", bgColor, height, width, position)}/>
     )
 }
