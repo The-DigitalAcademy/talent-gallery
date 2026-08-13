0ac6ed262ab016b346304f16cc85fd1518efde79
Simphiwe Nkabinde <simphiwenkabinde.dev@gmail.com>
2026-08-14 00:07:59 +0200

    add metadata to landing page
    * show unpublished talents on admin
    
    * align candidate container (#55)
    
    * hide link icon & functionlity for projects with empty url field
    
    * email: use logo with icon, update default email
    
    * add metadata to landing page
    
    ---------
    
    Co-authored-by: kg-manana <kgomotso@thedigitalacademy.co.za>

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,32 @@ import HowItWorks from "./_components/landing/HowItWorks";
 import CandidateStatuses from "./_components/landing/CandidateStatuses";
 import BrowseTalentPreview from "./_components/landing/BrowseTalentPreview";
 import TrustedBy from "./_components/landing/TrustedBy";
+import { Metadata } from "next";
+
+// SEO GENERATOR
+export async function generateMetadata(): Promise<Metadata> {
+  const title = 'TALENT Directory'
+  const description = "Discover and connect with top-tier, specialized creative professionals and tech talents."
+  const imageUrl = "https://w4u9ywo6wdd8vjiq.public.blob.vercel-storage.com/shaper-icon-brand.png";
+
+  return {
+    title,
+    description,
+    alternates: { canonical: "/" },
+    openGraph: {
+      title,
+      description,
+      images: [ { url: imageUrl, alt: "Shaper TALENT Brand" } ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [imageUrl],
+    },
+  };
+}
 
 export default async function LandingPage() {
   const supabase = await createClient();
